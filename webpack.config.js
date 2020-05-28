9f0c01589c908cde3909bf83b0911b41ae570853
Artur <nikitsin.artur@gmail.com>
2020-05-28 21:15:18 +0300

    feature: landing structure create, header create

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'bundles')
+    },
+    module: {
+        rules: [
+            {
+            test:/\.(s*)css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader',
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ]
+};
