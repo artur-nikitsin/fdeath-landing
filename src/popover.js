8466faafe96843dadf9751ed8a4594961c170555
Artur <nikitsin.artur@gmail.com>
2020-06-16 15:05:40 +0300

    refactor: popover functional refactor and complete

--- src/popover.js
+++ src/popover.js
@@ -1,94 +1,90 @@
 import $ from "jquery";
 
-
 let team = [{
     id: "Clara",
-    text: "rwerwerfrsgrger"
+    text: "rwerwerfrsgrger",
+    direction: "UI Designer"
 }, {
     id: "Sam",
-    text: "rfryjryjrtysgrger"
+    text: "rfryjryjrtysgrger",
+    direction: "UI Designer"
 }, {
     id: "Ryan",
-    text: "rfrsg25d2dft54rger"
+    text: "rfrsg25d2dft54rger",
+    direction: "UI Designer"
 }, {
     id: "Jake",
-    text: "rfrs4rtwrtfwergrger"
+    text: "rfrs4rtwrtfwergrger",
+    direction: "UI Designer"
 }, {
     id: "Zara",
-    text: "rfrs24yb3v6ertgsdfgrger"
+    text: "rfrs24yb3v6ertgsdfgrger",
+    direction: "UI Designer"
 }, {
     id: "Zina",
-    text: "rfr2d34d24d23r4wsgrger"
+    text: "rfr2d34d24d23r4wsgrger",
+    direction: "UI Designer"
 }];
 
 
 $(function () {
+    
+    $(".person-item").on("mouseenter", function (e) {
 
-
-    $(".person-item").hover(function (e) {
-
-        $('[data-toggle="popover"]').popover("hide");
-
+        let _this = this;
         let personName = "";
+        let personDirection = "";
         let aboutPerson = "";
         let social = "";
-
         let currentPerson = e.target.getAttribute("id")
-
 
         team.forEach(function (item, i) {
 
             if (item.id === currentPerson) {
 
                 personName = item.id;
+                personDirection = item.direction;
                 aboutPerson = item.text;
             }
         })
 
 
         $(this).popover({
-            trigger: 'manual',
-            animation: true,
+            trigger: "manual",
+            html: true,
+            animation: false,
             content: " ",
             template: '<div class="popover" ' +
                 'role="tooltip">' +
                 '<div class="arrow"></div>' +
-                '<h3 class="popover-title">' + personName + '</h3>' +
+                '<h3 class="popover-title">' + personName + '<span>' + personDirection + '<span></h3>' +
                 '<p class="popover-text">' + aboutPerson + '</p>' +
                 '<ul class="popover-social">' +
-                '<li><i class="fab fa-facebook-f"></i><li/> ' +
-                '<li><i class="fab fa-twitter"></i><li/> ' +
-                '<li><i class="fas fa-basketball-ball"></i><li/> ' +
-                '<li><i class="far fa-envelope"></i><li/> ' +
+                '<li><a href=""><i class="fab fa-facebook-f"></a></i><li/> ' +
+                '<li><a href=""><i class="fab fa-twitter"></i></a><li/> ' +
+                '<li><a href=""><i class="fas fa-basketball-ball"></a></i><li/> ' +
+                '<li><a href=""><i class="far fa-envelope"></i></a><li/> ' +
 
                 '</ul>' +
                 '<div class="popover-body"></div>' +
                 '</div>'
-        });
+        })
+
 
         $(this).popover("show");
 
-    });
+        $(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
 
+    }).on("mouseleave", function () {
+        let _this = this;
 
-    $(".person-item").mouseout(function (e) {
-
-        let nextElement = e.relatedTarget.getAttribute("class");
-
-        console.log(nextElement);
-        if (nextElement !== "popover bs-popover-bottom fade show" && nextElement !== "popover-body" &&
-            nextElement !== "arrow" && nextElement !== "popover-text" && nextElement !== "popover-social" &&
-            nextElement !== "popover-title" &&
-            nextElement !== "popover-social" &&
-            nextElement !== "fa-facebook-f" &&
-            nextElement !== "fa-twitter" &&
-            nextElement !== "fa-basketball-ball" &&
-            nextElement !== "fa-envelope"&&
-            nextElement !== null) {
-            $(this).popover("hide");
+        if (!$(".popover:hover").length) {
+            $(_this).popover("hide");
         }
 
     });
 
-
 })
+;
